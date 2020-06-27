7a8614e5af352b29d409bb7407894c56086c8a3b
nicholas chikore <nickchikore@gmail.com>
2020-06-27 13:26:02 +0100

    first commit

---
+++ design263.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using design263.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace design263.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
